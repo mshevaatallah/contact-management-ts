57fae77159f4b0559a17ac23b968bafab9ce328d
mshevaatallah <muhammadshax07@gmail.com>
2024-07-26 19:45:20 +0700

    add create contact feature

---
+++ src/validation/contact-validation.ts
@@ -0,0 +1,10 @@
+import { z, ZodType } from "zod";
+
+export class ContactValidation {
+  static readonly CREATE: ZodType = z.object({
+    firstName: z.string().min(3).max(50),
+    lastName: z.string().min(3).max(50).optional(),
+    email: z.string().email().optional(),
+    phone: z.string().min(1).max(20).optional(),
+  });
+}
